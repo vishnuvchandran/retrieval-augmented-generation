2387003d660ef4a521353a4e23ce323cdde90652
Vishnu V A <vishnu@PIC4513.pitsolutions.com>
2024-09-27 17:34:06 +0530

    Indexing table schema on page load

---
+++ utils/schema_index.py
@@ -0,0 +1,56 @@
+import json
+from langchain.schema import Document
+from langchain.vectorstores import FAISS
+from utils.database import get_db_connection
+from utils.llm_selection import get_embedding_model
+
+class SchemaIndexer:
+    def __init__(self):
+        self.pgdb = get_db_connection()
+        self.embedding_model = get_embedding_model('google')
+        self.db = self._create_index()
+
+
+    def _fetch_schema_from_db(self):
+        cursor = self.pgdb.cursor()
+        schema_query = """
+        SELECT 
+            t.table_name,
+            json_agg(json_build_object(
+                'name', c.column_name,
+                'type', c.data_type
+            ))::text AS columns
+        FROM 
+            information_schema.tables t
+        JOIN 
+            information_schema.columns c ON t.table_name = c.table_name
+        WHERE 
+            t.table_schema = 'public'
+        GROUP BY 
+            t.table_name
+        """
+        cursor.execute(schema_query)
+        schema = cursor.fetchall()
+        cursor.close()
+        
+        documents = []
+        for table in schema:
+            doc = Document(
+                page_content=json.dumps({
+                    "table_name": table[0],
+                    "columns": json.loads(table[1])
+                }),
+                metadata={"table": table[0]}
+            )
+            documents.append(doc)
+        
+        return documents
+
+
+    def _create_index(self):
+        documents = self._fetch_schema_from_db()
+        return FAISS.from_documents(documents=documents, embedding=self.embedding_model)
+
+
+    def get_index(self):
+        return self.db
